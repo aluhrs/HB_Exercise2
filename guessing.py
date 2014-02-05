9d12787ac565cea4e6ed8901382cb968e47aacd4
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 09:40:37 -0800

    Guessing game committed with limit of 9 attempts.

--- guessing.py
+++ guessing.py
@@ -3,14 +3,20 @@ print "Hey you, what is your name?"
 name = raw_input("Type your name: "), 
 print "Lets play a game." 
 print " %s, I am thinking of a number between 1 and 100. Try to guess my number" % name
-
+attempts = 0
 generated_integer = random.randint(1,100)
 
+#guessing loop
 while True:
 
     your_guess = raw_input("Your Guess: ")
+    attempts = attempts + 1
+    if attempts > 9:
+        print "You have exceeded the limit of 9 attempts."
+        break
 
-    if not your_guess.isdigit():
+
+    elif not your_guess.isdigit():
         print "Try again. Enter a number between 1 and 100."
 
     elif int(your_guess) < 1 or int(your_guess) > 100:
@@ -23,5 +29,5 @@ while True:
         print "You've guessed too high, guess again:"
 
     else:
-        print "You Win!"
+        print "You win with %d attempts!"  % attempts
         break
