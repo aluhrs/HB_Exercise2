30d25462f1dba7fce42d51175afe1aba9d52f519
Hackbright Student <info@hackbrightacademy.com>
2014-02-05 16:18:34 -0800

    Added Addition

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
+import arithmetic
 # repeat forever
 while True:
     # read input
     token = raw_input("> ")
     # tokenize input
-    tokens = token.split(" ")
-    print tokens
+    tokens = token.split(" ")   
+    #print tokens
+
+    if tokens[0] == "q":
+        break
+    #if tokens[1].isdigit() and tokens[2].isdigit():
+     #   int(tokens)
+
+    #addition
+    if tokens[0] == "+":    
+        add = arithmetic.add(int(tokens[1]), int(tokens[2]))
+        print add
     break
+
